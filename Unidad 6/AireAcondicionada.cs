f49a0a48dc4ad0428a89449e4a5a563c2b05ddea
Yola19 <107368952+informatica042003@users.noreply.github.com>
2022-12-05 11:33:44 -0400

    update

---
+++ Unidad 6/AireAcondicionada.cs
@@ -0,0 +1,9 @@
+using System;
+class AireAcondicionado{
+    private string opcion;
+    private string marca;
+    private int velocidad;
+    private int temperatura;
+
+    public AireAcondicionado()
+}
