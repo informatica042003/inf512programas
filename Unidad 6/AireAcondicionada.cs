635a658be4317a06345855257114cb771cfc3686
Yola19 <107368952+informatica042003@users.noreply.github.com>
2022-12-05 11:49:48 -0400

    upgrade

--- Unidad 6/AireAcondicionada.cs
+++ Unidad 6/AireAcondicionada.cs
@@ -2,8 +2,16 @@ using System;
 class AireAcondicionado{
     private string opcion;
     private string marca;
-    private int velocidad;
-    private int temperatura;
+    private int velocidad= 1;
+    private int temperatura= 17;
 
-    public AireAcondicionado()
+    public AireAcondicionado(int velocidad, int temperatura){
+        velocidad= this.velocidad;
+        temperatura= this.temperatura;
+
+        Console.WriteLine("El AC se ha encendido.\nTemperatura: " + temperatura + 
+        " *C \nVelocidad: " + velocidad + "\nEstado: Encendido");
+    }
+
+    public void apa
 }
